a1a82ed48e1852ae501a1de8dcfb28a29cf42ca4
Samuele Bacchetta <samuele1988@gmail.com>
2017-02-03 12:03:35 +0100

    Emails working

--- pages/emailView.php
+++ pages/emailView.php
@@ -7,10 +7,10 @@
 <?php 
 if(isset($_GET['action'])){
 	if($_GET['action']=="new")
-		CreateMailForm(0);
+		CreateMailForm($_GET['action']);
 	if($_GET['action']=="received"){
 		$email = GetEmailDetail($_GET['id']);
-		print_r($email);
+		DisplayMail($email);
 		$query = "UPDATE emails SET letto=1 WHERE id=".$email['id'];
 		SendQuery($query);
 	}
@@ -25,34 +25,67 @@ if(isset($_GET['action'])){
 	}
 }
 if(isset($_POST['Submit'])){
-	if($_POST['Submit']=="Invia")
+	if($_POST['Submit']=="Invia"){
 		$uId = GetUserId();
 		$query = 	"INSERT INTO emails (idMittente, idDestinatario, oggetto, testo) ".
 					"VALUES ('".$uId."', '".$_POST['destinatario']."', '".$_POST['oggetto']."', '".$_POST['testo']."')";
 		SendQuery($query);
+	}
+	if($_POST['Submit']=="Rispondi"){
+		CreateAnswerForm($_POST['emailId']);
+	}
 }
 ?>
 </body>
 </html>
 
 <?php
-function CreateMailForm($action){
-	echo	"<form id='newMail' action='?action=Invia' method='post' accept-charset='UTF-8'>\n";
-	echo 	"<fieldset>\n";
-	echo 	"<legend>Nuova email</legend>\n";
-	echo	"<label for='courseName'>A: </label>\n";
-	echo	"<select name='destinatario'>\n";
-	CreateUserSelect();
-	echo 	"</select>\n";
+function DisplayMail($email){
+	$mittente = GetUserDetail($email['idMittente']);
+	$destinatario = GetUserDetail($email['idDestinatario']);
+	echo "<h2>".$email['oggetto']."</h2>";
+	echo "<h3>".$mittente['cognome']." ".$mittente['nome']."</h3>";
+	echo "<p>Inviato: ".$email['data']."</p>";
+	echo "<br>";
+	echo "<p>".$email['testo']."</p>";
+	echo "<br>";
+	echo "<form id='answerMail' action='?action=Rispondi' method='post' accept-charset='UTF-8'>\n";
+	echo "<input type='hidden' name='emailId' value='".$email['id']."'>";
+	echo "<input type='submit' name='Submit' value='Rispondi'/>\n";
+	echo "</form>";
+}
+function CreateAnswerForm($mailId){
+	$email = GetEmailDetail($mailId);
+	$destinatario = GetUserDetail($email['idMittente']);
+	echo	"<form id='answerMail' action='?action=Rispondi' method='post' accept-charset='UTF-8'>\n";
+	echo 	"<legend><strong>Rispondi</strong></legend>\n";
+	echo	"<label>A: ".$destinatario['cognome']." ".$destinatario['nome']."</label><br>";
+	echo	"<label>Oggetto: </label>\n";
+	echo 	"<input type='text' name='oggetto' id='oggetto' value='Re: ".$email['oggetto']."'> \n";
 	echo	"<br>\n";
-	echo	"<label for='time' >Oggetto: </label>\n";
-	echo 	"<input type='text' name='oggetto' id='oggetto' />\n";
+	echo 	"<textarea name='testo' rows='10' cols='30'>".$email['testo']."</textarea>";
 	echo	"<br>\n";
-	echo 	"<input type='text' name='testo' id='testo' />\n";
-	echo	"<br>\n";
+	echo 	"<input type='hidden' name='destinatario' value='".$email['idMittente']."'>";
 	echo	"<input type='submit' name='Submit' value='Invia' />\n";
-	echo	"</fieldset>\n";
 	echo	"</form>\n";
+}
+function CreateMailForm($action){
+	if ($action == "new"){
+		echo	"<form id='newMail' action='?action=Invia' method='post' accept-charset='UTF-8'>\n";
+		echo 	"<legend><strong>Nuova email</strong></legend>\n";
+		echo	"<label for='courseName'>A: </label>\n";
+		echo	"<select name='destinatario'>\n";
+		CreateUserSelect();
+		echo 	"</select>\n";
+		echo	"<br>\n";
+		echo	"<label for='time' >Oggetto: </label>\n";
+		echo 	"<input type='text' name='oggetto' id='oggetto' />\n";
+		echo	"<br>\n";
+		echo 	"<textarea name='testo' rows='10' cols='30'></textarea>";
+		echo	"<br>\n";
+		echo	"<input type='submit' name='Submit' value='Invia' />\n";
+		echo	"</form>\n";
+	}
 }
 
 function CreateUserSelect(){
@@ -67,40 +100,10 @@ function GetEmailDetail($mailId){
 	$query = "SELECT * FROM emails WHERE id='".$mailId."' ORDER BY data ASC";
 	return mysqli_fetch_assoc(SendQuery($query));
 }
-/*
-function GetUserId(){
-	$userDetail = json_decode($_COOKIE['user'], true);
-	$userName = $userDetail['user'];
 
-	$query = "SELECT id FROM users WHERE username='".$userName."'";
-
-	$result = SendQuery($query);
-
-	if($result){
-		$val = mysqli_fetch_assoc($result);
-		return $val['id'];
-	}
-	return "nope";
+function GetUserDetail($id){
+	$query = "SELECT u.nome, u.cognome FROM users AS u WHERE id=".$id;
+	return mysqli_fetch_assoc(SendQuery($query));
 }
-
-function SendQuery($query){
-	$servername = "localhost";
-	$usernameDb = "webuser";
-	$passwordDb = "webpassword";
-	$dbname = "virtualcampus";
-
-	$conn = new mysqli($servername, $usernameDb, $passwordDb, $dbname);
-
-	if (!$conn) {
-		return false;
-	}
-
-	$result = $conn->query($query);
-
-	$conn->close();
-
-	return $result;
-}
-*/
 ?>
 
